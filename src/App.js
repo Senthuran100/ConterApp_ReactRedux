2b821e7cc49f3b53edf1d08da49b3773a174c0ab
Senthuran Manoharan <senthuran.manoharan@gmail.com>
2020-08-25 09:06:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import store,{COUNTER_DECREMENT,COUNTER_INCREMENT} from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      hello:'hello'
+    }
+  }
+
+  increment=()=>{
+    
+    store.dispatch({type:COUNTER_INCREMENT});
+    console.log(store.getState());
+  }
+   decrement=()=>{
+    store.dispatch({type:COUNTER_DECREMENT});
+    console.log(store.getState());
+  }
+
+  push=()=>{
+    
+    store.dispatch({type:'push',value:Math.random()});
+    console.log(store.getState());
+  }
+
+  pop=()=>{
+    
+    store.dispatch({type:'pop'});
+    console.log(store.getState());
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.hello}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <button onClick={this.push}>Push</button>
+        <button onClick={this.pop}>Pop</button>
+
     </div>
-  );
+    )
+  }
 }
-
-export default App;
---
+++ src/store.js
@@ -0,0 +1,52 @@
+import * as redux from 'redux';
+
+// Actions
+export const COUNTER_INCREMENT = 'counter/increment';
+export const COUNTER_DECREMENT  = 'counter/decrement';
+
+const countinitialState ={
+    count:0
+}
+const countreducer = (state=countinitialState,action) =>{
+    switch(action.type){
+        case COUNTER_INCREMENT: {
+            return {...state,count:state.count+1};
+        }
+        case COUNTER_DECREMENT: {
+            return {...state,count:state.count-1};
+        }
+        default:{
+            return state;
+        }
+    }
+}
+
+const testreducer = (state=[],action) =>{
+    switch(action.type){
+        case 'push': {
+            const data = [...state]
+            data.push(action.value)
+            return [...data];
+        }
+        case 'pop': {
+            const data = [...state]
+            data.pop(action.value)
+            return [...data];
+        }
+        default:{
+            return state;
+        }
+    }
+}
+
+const rootReducer= (state={},action) => {
+    return {
+        counter :countreducer(state.counter,action),
+        tester : testreducer(state.tester,action)
+    }
+}
+
+
+const store = redux.createStore(rootReducer);
+
+export default store;
