ec072399cf0a6846955bec343da1f67f7cc255a5
Senthuran Manoharan <senthuran.manoharan@gmail.com>
2020-09-02 11:00:04 +0530

    Modifying the search component

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss';
 
 export default function Search() {
+    const [content, setContent] = useState("");
+
+    const handleChange = (e) => {
+        setContent(e.target.value);
+    }
     return (
         <div className="search">
-             <input type="text" placeholder="Search.."></input>
-             <button type="submit">Submit</button>
+            <h1>{content}</h1>
+            <input type="text" placeholder="Type Content ...." name='content11' onChange={handleChange}></input>
         </div>
     )
 }
